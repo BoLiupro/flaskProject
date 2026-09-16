b0a82157cf9d945582e3055e59822f63406efc4a
Bo Liu <94628811+BoLiupro@users.noreply.github.com>
2026-09-16 13:22:12 +0800

    security: move database credentials to environment variables

--- models/mysql.py
+++ models/mysql.py
@@ -1,9 +1,11 @@
+import os
 import pymysql
 
-conn = pymysql.connect(  # 创建数据库连接
-    host='localhost',  # 要连接的数据库所在主机ip
-    user='root',  # 数据库登录用户名
-    password='66666666',  # 登录用户密码
-    database='west2_takeout',  # 连接的数据库名，也可以后续通过cursor.execture('user test_db')指定
-    charset='utf8'  # 编码，注意不能写成utf-8
+conn = pymysql.connect(
+    host=os.getenv('TAKEOUT_DB_HOST', 'localhost'),
+    port=int(os.getenv('TAKEOUT_DB_PORT', '3306')),
+    user=os.getenv('TAKEOUT_DB_USER', 'root'),
+    password=os.getenv('TAKEOUT_DB_PASSWORD', ''),
+    database=os.getenv('TAKEOUT_DB_NAME', 'west2_takeout'),
+    charset='utf8'
 )
