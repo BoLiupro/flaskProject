8ab8b26964718c934fb9eaa382039c605a029d6f
Bo Liu <94628811+BoLiupro@users.noreply.github.com>
2026-09-16 13:22:18 +0800

    security: move mail credentials to environment variables

--- my_mail.py
+++ my_mail.py
@@ -1,16 +1,13 @@
-
-MAIL_SERVER	="smtp.qq.com"
-MAIL_PORT	=465
-MAIL_USE_TLS	= False
-MAIL_USE_SSl	= True
-MAIL_DEBUG	= True
-MAIL_USERNAME	="1249237461@qq.com"
-MAIL_PASSWORD	="jlfxckkjrwexfdii"
-MAIL_DEFAULT_SENDER="1249237461@qq.com"
-
+import os
 from flask_mail import Mail
 
-mail=Mail()
+MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.qq.com")
+MAIL_PORT = int(os.getenv("MAIL_PORT", "465"))
+MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "false").lower() == "true"
+MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "true").lower() == "true"
+MAIL_DEBUG = os.getenv("MAIL_DEBUG", "false").lower() == "true"
+MAIL_USERNAME = os.getenv("MAIL_USERNAME", "")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
+MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", MAIL_USERNAME)
 
-
-
+mail = Mail()
